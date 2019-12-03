ca1c2a61bdbf630fac6c34f95ebf15168133f278
Maciej Soltysiak <pysiak@gmail.com>
2019-12-03 18:03:51 +0100

    Day1

---
+++ day1a/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace day1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                int sum = 0;
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    sum += CalculateFuel(int.Parse(line));
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+
+        static int CalculateFuel(int mass)
+        {
+            int fuel = (int) Math.Floor((decimal) mass / 3) - 2;
+            //Console.Write(fuel);
+            //Console.ReadKey();
+            return fuel;
+        }
+    }
+}
---
+++ day1b/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace day1b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                int sum = 0;
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    int fuel = CalculateFuel(int.Parse(line));
+                    //Console.WriteLine(fuel);
+                    sum += fuel;
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+
+        static int CalculateFuel(int mass)
+        {
+            int fuel = (int) Math.Floor((decimal) mass / 3) - 2;
+            if (fuel > 0)
+            {
+                fuel += CalculateFuel(fuel);
+            }
+
+            if (fuel < 0)
+                fuel = 0;
+            //Console.Write(fuel);
+            //Console.ReadKey();
+            return fuel;
+        }
+    }
+}
